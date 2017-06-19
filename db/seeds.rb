94f5ce3f678707d70e9568e118e6a6e9f7532bcf
Marketa Dragounova <marketa.dragounova@gmail.com>
2017-06-19 11:42:59 +0800

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+articles = Article.create(title: Faker::Name.title, content: Faker::Lorem.paragraph(30))
+end
+
+
